1ab00d9a39307823b03de5ef96756038b3221559
Miguel Somohano <miguel.spinis@gmail.com>
2020-06-21 03:38:48 -0500

    Agregando presupueto del usuario - Pregunta.js

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,14 +1,44 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Pregunta = () => {
+
+    // Creando el state
+    const [ cantidad, guardarCantidad ] = useState(0);
+
+
+    // Función para leer el presupuesto
+    // colocamos e para acceder a los valores, como e.target, etc.
+    const definirPresupuesto = e => {
+        // Para que no me arroje los valores de entrada como strings, hacemos el parse
+        //console.log(parseInt(e.target.value)); el sugundo argumento declara la base numerica
+        guardarCantidad( parseInt(e.target.value, 10) )
+    }
+
+    // Submit para definir presupuesto
+    // para prevenir que se envie el query a la caja de urls en el navegador
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        // Validar
+
+        // Paso de validacion
+    }
+
+    
+
     return ( 
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
-            <form>
+            <form
+                // Evento para el submit
+                onSubmit={agregarPresupuesto}
+            >
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    // Evento
+                    onChange={definirPresupuesto}
                 />
                 <input
                     type="submit"
