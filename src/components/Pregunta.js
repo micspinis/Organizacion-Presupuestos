1306bd1eb9fbfd643362991cfd42082dc51be242
Miguel Somohano <miguel.spinis@gmail.com>
2020-06-21 04:19:28 -0500

    Creando carga condicional de componentes - Pregunta - Formulario

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import Error from './Error'
 
-const Pregunta = ({ guardarPresupuesto, guardarRestante }) => {
+const Pregunta = ({ guardarPresupuesto, guardarRestante, actualizarPregunta }) => {
 
     // Creando el state
     const [ cantidad, guardarCantidad ] = useState(0);
@@ -32,6 +32,7 @@ const Pregunta = ({ guardarPresupuesto, guardarRestante }) => {
         guardarError(false);
         guardarPresupuesto(cantidad);
         guardarRestante(cantidad);
+        actualizarPregunta(false);
     }
 
     
