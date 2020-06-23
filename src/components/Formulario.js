185330421266752c79995337cf5d1f10ac616efd
Miguel Somohano <miguel.spinis@gmail.com>
2020-06-22 23:11:01 -0500

    Creando state para los gastos

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,28 @@
 import React, { useState } from 'react';
 
 const Formulario = () => {
+
+    const [ nombre, guardarNombre ] = useState('');
+    const [ cantidad, guardarCantidad ] = useState(0);
+
+    // cuando el usuario agrega un gasto
+    const agregarGasto = e => {
+        e.preventDefault();
+
+        // Validar
+
+
+        // construir el gasto
+
+        // pasar el gasto al componente principal
+
+        // resetar el form
+    }
+
     return ( 
-        <form>
+        <form
+            onSubmit={agregarGasto}
+        >
             <h2>Agrega tus gastos aqui</h2>
             <div className="campo">
                 <label>Nombre Gasto</label>
@@ -10,6 +30,8 @@ const Formulario = () => {
                     type="text"
                     className="u-full-width"
                     placeholder="Ej. Transporte"
+                    value={nombre}
+                    onChange={e => guardarNombre(e.target.value)}
                 />
             </div>
             <div className="campo">
@@ -18,6 +40,8 @@ const Formulario = () => {
                     type="number"
                     className="u-full-width"
                     placeholder="Ej. 300"
+                    value={cantidad}
+                    onChange={e => guardarCantidad(parseInt(e.target.value, 10))}
                 />
             </div>
             <input
